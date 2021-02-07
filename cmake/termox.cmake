4ce00fd9b3abdd94a05f07108c414d5b11d4f6c7
Mark Melton <mark.melton@notlem.com>
2021-02-07 12:02:23 -0500

    Added ncurses as target library for termox.

--- cmake/termox.cmake
+++ cmake/termox.cmake
@@ -3,3 +3,6 @@ cmake_minimum_required (VERSION 3.12 FATAL_ERROR)
 add_library(TermOx STATIC IMPORTED)
 set_target_properties(TermOx PROPERTIES IMPORTED_LOCATION ${DEPENDS_INSTALL_DIR}/lib/libTermOx.a)
 target_include_directories(TermOx INTERFACE ${DEPENDS_INSTALL_DIR}/include)
+
+add_library(TermOxCurses STATIC IMPORTED)
+set_target_properties(TermOxCurses PROPERTIES IMPORTED_LOCATION ${DEPENDS_INSTALL_DIR}/lib/libncursesw.a)
