e48cfcf94e51f0926548e7cb7dc78a9d69f5e1d1
Mark Melton <mark.melton@notlem.com>
2021-02-07 11:51:27 -0500

    WIP. Adding TermOx dependency.

---
+++ cmake/termox.cmake
@@ -0,0 +1,5 @@
+cmake_minimum_required (VERSION 3.12 FATAL_ERROR)
+
+add_library(TermOx STATIC IMPORTED)
+set_target_properties(TermOx PROPERTIES IMPORTED_LOCATION ${DEPENDS_INSTALL_DIR}/lib/libTermOx.a)
+target_include_directories(TermOx INTERFACE ${DEPENDS_INSTALL_DIR}/include)
