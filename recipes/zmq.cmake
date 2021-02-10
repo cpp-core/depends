3a22e35277ccd9aba5b3da1476e4178f81796186
Mark Melton <mark.melton@notlem.com>
2021-02-09 21:48:57 -0500

    Updated libzmq and cppzmq forks.

--- recipes/zmq.cmake
+++ recipes/zmq.cmake
@@ -6,7 +6,6 @@ if(NOT TARGET cxx_cppzmq)
   ExternalProject_Add(cxx_libzmq
     PREFIX ${DEPENDS_BUILD_DIR}/libzmq
     GIT_REPOSITORY git@github.com:melton1968/libzmq.git
-    GIT_TAG 5888b2e319fb7afe20f920e93928a7e96a5a2f5c
     GIT_SHALLOW TRUE
     EXCLUDE_FROM_ALL TRUE
     CONFIGURE_COMMAND CC=${CC} CXX=${CXX} ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${DEPENDS_INSTALL_DIR} -DWITH_TLS=OFF ../cxx_libzmq
@@ -16,7 +15,6 @@ if(NOT TARGET cxx_cppzmq)
   ExternalProject_Add(cxx_cppzmq
     PREFIX ${DEPENDS_BUILD_DIR}/cppzmq
     GIT_REPOSITORY git@github.com:melton1968/cppzmq.git
-    GIT_TAG c6ddc504d16a05f428a248784107eddd33ae5e14
     GIT_SHALLOW TRUE
     EXCLUDE_FROM_ALL TRUE
     CONFIGURE_COMMAND CC=${CC} CXX=${CXX} ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${DEPENDS_INSTALL_DIR} ../cxx_cppzmq
