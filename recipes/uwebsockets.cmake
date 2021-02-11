0a71ecee64eb4aa28a6feb7d1898f493bea2a0e4
Mark Melton <mark.melton@notlem.com>
2021-02-11 11:32:06 -0500

    Updated cppzmq recipes and update uWebSockets tag.

--- recipes/uwebsockets.cmake
+++ recipes/uwebsockets.cmake
@@ -6,7 +6,7 @@ if(NOT TARGET cxx_uwebsockets)
     BUILD_IN_SOURCE 1
     PREFIX ${DEPENDS_BUILD_DIR}/uwebsockets
     GIT_REPOSITORY git@github.com:melton1968/uWebSockets.git
-    GIT_TAG a17a6a7268ee6d17c7aec51afa27264c1981aec6
+    GIT_TAG 0950c2a84f30c07e4cd40ea697a88800ed3609c1
     GIT_SHALLOW TRUE
     EXCLUDE_FROM_ALL TRUE
     CONFIGURE_COMMAND ""
