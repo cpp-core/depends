b063a4e884c2210e0011853d4a77c67d42ecb3e0
Mark Melton <mark.melton@notlem.com>
2021-02-16 03:06:53 +0000

    Look for shared cpr library.

--- cmake/cpr.cmake
+++ cmake/cpr.cmake
@@ -1,6 +1,5 @@
 cmake_minimum_required (VERSION 3.10 FATAL_ERROR)
 
 add_library(cpr STATIC IMPORTED)
-set_target_properties(cpr PROPERTIES IMPORTED_LOCATION ${DEPENDS_INSTALL_DIR}/lib/libcpr.a)
+set_target_properties(cpr PROPERTIES IMPORTED_LOCATION ${DEPENDS_INSTALL_DIR}/lib/libcpr.so)
 target_include_directories(cpr INTERFACE ${DEPENDS_INSTALL_DIR}/include)
-
