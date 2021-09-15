a59430bfaee5b3bea36e085f7f87793f6fa99d51
Mark Melton <mark.melton@notlem.com>
2021-09-15 13:04:44 -0400

    Update json version.

--- recipes/json.cmake
+++ recipes/json.cmake
@@ -5,7 +5,7 @@ if(NOT TARGET cxx_json)
   ExternalProject_Add(cxx_json
     PREFIX ${DEPENDS_BUILD_DIR}/nlohmann_json
     GIT_REPOSITORY git@github.com:melton1968/json.git
-    GIT_TAG tags/v3.7.0
+    GIT_TAG tags/v3.9.1
     GIT_SHALLOW TRUE
     EXCLUDE_FROM_ALL TRUE
     CONFIGURE_COMMAND CC=${CC} CXX=${CXX} CXXFLAGS=${CXXFLAGS} ${CMAKE_COMMAND} -DJSON_BuildTests=OFF -DCMAKE_INSTALL_PREFIX=${DEPENDS_INSTALL_DIR} ../cxx_json
