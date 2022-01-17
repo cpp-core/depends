6cb595b65fbffa53052c66238e4b20afd3ac7313
Mark Melton <mark.melton@notlem.com>
2022-01-17 15:06:29 -0500

    Updated cpr installation.

--- cmake/cpr.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required (VERSION 3.10 FATAL_ERROR)
-
-add_library(cpr STATIC IMPORTED)
-set_target_properties(cpr
-  PROPERTIES IMPORTED_LOCATION ${DEPENDS_INSTALL_DIR}/lib/libcpr${CMAKE_SHARED_LIBRARY_SUFFIX})
-target_include_directories(cpr INTERFACE ${DEPENDS_INSTALL_DIR}/include)
