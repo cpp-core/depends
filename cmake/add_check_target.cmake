1534fdc5f12d6687af778650af3336602d672878
Mark Melton <mark.melton@notlem.com>
2022-09-12 17:40:42 -0400

    Made check target dependent on build without install.

--- cmake/add_check_target.cmake
+++ cmake/add_check_target.cmake
@@ -1,8 +1,8 @@
 cmake_minimum_required(VERSION 3.22 FATAL_ERROR)
 
 function(add_check_target TARGET)
-  add_custom_target(${TARGET}_check
+  add_custom_target(${TARGET}-check
     COMMAND make check
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/recipes/${TARGET}/${TARGET}/src/${TARGET}-build)
-  add_dependencies(${TARGET}_check ${TARGET})
+  add_dependencies(${TARGET}-check ${TARGET}-build)
 endfunction()
